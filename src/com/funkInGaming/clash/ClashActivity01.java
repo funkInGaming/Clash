b7769b463ef725fc061be12b19b1c17e46be51b9
Marcellus Castelino <marcelrainer@gmail.com>
2014-11-06 12:04:44 +0530

    Clash Ver 1.0
    First set of source files for Clash.

---
+++ src/com/funkInGaming/clash/ClashActivity01.java
@@ -0,0 +1,15 @@
+package com.funkInGaming.clash;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ClashActivity01 extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.reg_activity);
+    }
+}
---
+++ src/com/funkInGaming/clash/MainActivity.java
@@ -0,0 +1,9 @@
+package com.funkInGaming.clash;
+
+import android.app.Activity;
+
+/**
+ * Created by castelin on 30-10-2014.
+ */
+public class MainActivity extends Activity{
+}
